659b1c364b5dc5ff48d3e3cf26dd13e61833bd28
Abe Kohandel <abe.kohandel@gmail.com>
2023-08-03 22:48:36 -0700

    Enforce install of octo-nvim dependencies

--- lua/custom/plugins/octo-nvim.lua
+++ lua/custom/plugins/octo-nvim.lua
@@ -1,9 +1,9 @@
 return {
   'pwntester/octo.nvim',
-  requires = {
+  dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
-    'kyazdani42/nvim-web-devicons',
+    'nvim-tree/nvim-web-devicons',
   },
   config = function ()
     require"octo".setup()
