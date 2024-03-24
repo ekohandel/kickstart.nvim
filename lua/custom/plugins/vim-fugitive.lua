50eb04fa3b89468d907e9b3c5ac07f44d0188aab
Abe Kohandel <abe.kohandel@gmail.com>
2024-03-24 02:53:20 +0000

    Add Zoox GitHub URL

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -2,5 +2,9 @@ return {
   'tpope/vim-fugitive',
   dependencies = {
     'tpope/vim-rhubarb',
+
+    config = function()
+      vim.g.github_enterprise_urls = { 'https://git.zooxlabs.com' }
+    end,
   },
 }
